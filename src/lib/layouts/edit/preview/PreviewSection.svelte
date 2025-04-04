c1a5ff6aa576c80a37b4ff92ddad143c5f5c99a7
daylily <xy.r@outlook.com>
2025-04-03 22:50:36 -0400

    Remove extraneous prop

--- src/lib/layouts/edit/preview/PreviewSection.svelte
+++ src/lib/layouts/edit/preview/PreviewSection.svelte
@@ -2,17 +2,15 @@
   import FileSelect from './FileSelect.svelte'
   import PreviewCanvas1x from './PreviewCanvas1x.svelte'
   import PreviewCanvas2x from './PreviewCanvas2x.svelte'
-
-  let fileInputEl: HTMLInputElement | null = $state(null)
 </script>
 
 <section class="stack gap-4" aria-labelledby="preview-section-label">
   <h2 class="font-semibold text-xl/6" id="preview-section-label">Choose an image</h2>
 
-  <FileSelect bind:ref={fileInputEl} />
+  <FileSelect />
 
   <div class="stack-h gap-4 max-md:stack">
-    <PreviewCanvas2x {fileInputEl} />
+    <PreviewCanvas2x />
     <PreviewCanvas1x />
   </div>
 </section>
